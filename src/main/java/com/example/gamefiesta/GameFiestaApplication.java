f05771af2ede96cf4f57375d5f9a592df7506d49
Arut3nCodes <56273076+Arut3nCodes@users.noreply.github.com>
2023-11-05 14:54:33 +0100

    Initializing project in GitHub repository

---
+++ src/main/java/com/example/gamefiesta/GameFiestaApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamefiesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameFiestaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameFiestaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gamefiesta/GameFiestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamefiesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameFiestaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
