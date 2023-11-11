0042ea6800954cfb75a30e8792d2fb955b35cd64
Dagonin <Dagonins@gmail.com>
2023-11-11 11:34:50 +0100

    Zriobiony restController, odpala index.html na /

--- src/main/java/com/example/gamefiesta/WebCrontroller.java
+++ src/main/java/com/example/gamefiesta/WebCrontroller.java
@@ -1,9 +1,19 @@
 package com.example.gamefiesta;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @RestController
 public class WebCrontroller {
     
+
+    @RequestMapping("/")
+    public ModelAndView welcome() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index.html");
+        return modelAndView;
+    }  
 }
