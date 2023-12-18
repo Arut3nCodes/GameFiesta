5813c99c2832759a0a59cef51d8dc1fda7e78163
Arut3nCodes <56273076+Arut3nCodes@users.noreply.github.com>
2023-12-18 17:03:48 +0100

    Adding cookies and cookies filtering to project (page works after logging in)

--- src/main/java/com/example/gamefiesta/ClassicBracket.java
+++ src/main/java/com/example/gamefiesta/ClassicBracket.java
@@ -1,5 +1,76 @@
 package com.example.gamefiesta;
 
+import java.util.ArrayList;
+
 public class ClassicBracket extends Bracket{
+    ArrayList<Match> listOfMatchObjects;
+    public ClassicBracket(){
+        this.set_id("PogChamp");
+        this.setType("Classic");
+        this.setListOfMatches(new ArrayList<String>());
+        this.setListOfMatchObjects(new ArrayList<Match>());
+    }
+
+    public ArrayList<Match> getListOfMatchObjects() {
+        return listOfMatchObjects;
+    }
+
+    public void setListOfMatchObjects(ArrayList<Match> listOfMatchObjects) {
+        this.listOfMatchObjects = listOfMatchObjects;
+    }
+
+    //Na ten moment wersja bez bye'ów
+    //Budujemy drzewo binarne w liście, także
+    //Na ten moment bracket będzie przechowywany w bracketcie, pomijam bd
+    public void generateLadder(ArrayList<String> listOfTeams){
+        if(isPowerOfTwo(listOfTeams.size())) {
+            int sizeOfList = listOfTeams.size()-1;
+            for (int i = 0; i < sizeOfList; i++) {
+
+                listOfMatchObjects.add(new Match(Integer.toString(i)));
+                this.getListOfMatches().add(this.get_id() + i);
+
+                if (i >= sizeOfList / 2){
+                    int index = (i - sizeOfList/2) % ((sizeOfList+1)/2) * 2;
+                    this.getListOfMatchObjects().get(i).setTeamA(listOfTeams.get(index));
+                    this.getListOfMatchObjects().get(i).setTeamB(listOfTeams.get(index+1));
+                }
+            }
+        }
+        else{
+            System.out.println("There is not enough teams to form a bracket");
+        }
+    }
+
+    public void printLadder(){
+        if(!listOfMatchObjects.isEmpty()) {
+            for (Match m : listOfMatchObjects) {
+                System.out.println(m.toString());
+            }
+        }
+        else{
+            System.out.println("There are no matches to print");
+        }
+    }
+
+    private boolean isPowerOfTwo(int n)
+    {
+        if (n == 0)
+            return false;
+
+        return ((int)Math.ceil(Math.log(n) / Math.log(2)) == (int)Math.floor(Math.log(n) / Math.log(2)));
+    }
+
+    public Match getMatch(String matchId){
+        for(Match obj: getListOfMatchObjects()){
+            if(obj.get_id().equals(matchId)) {
+                System.out.println("Match found");
+                return obj;
+            }
+        }
+        System.out.println("Match with id: " + matchId + " was not found");
+        return null;
+    }
 
 }
+
--- src/main/java/com/example/gamefiesta/Match.java
+++ src/main/java/com/example/gamefiesta/Match.java
@@ -2,19 +2,56 @@ package com.example.gamefiesta;
 
 import java.util.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
 @Document
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Match {
     @Id
     private String _id;
     private String teamA;
     private String teamB;
+    private int scoreA;
+    private int scoreB;
+    private String matchWinner;
     private String status;
     private Date date;
     // przerobić żeby była lista "spotkań", spotkanie: TeamA, TeamB, Status, Data, a Match ma tylko tą liste i ID
+    public Match(String _id){
+        this._id = _id;
+        this.teamA = "TBD";
+        this.teamB = "TBD";
+        this.scoreA = 0;
+        this.scoreB = 0;
+        this.matchWinner = "TBD";
+        this.status = "NOT STARTED";
+    }
+
+    @Override
+    public String toString(){
+        if(this.getStatus().equals("NOT STARTED")) {
+            return "Match " + get_id() + ": " + getTeamA() + " vs " + getTeamB() + " | Status: " + getStatus() + getMatchWinner();
+        }
+        else{
+            return "Match " + get_id() + ": " + getTeamA() + " vs " + getTeamB() + " | Status: " + getStatus() + " | Score: " + getScoreA() + "-" + getScoreB() + " | Winner: " + getMatchWinner();
+        }
+    }
+
+    public String setVictor(){
+        return null;
+    }
+
+    public void addPointToTeamA(){
+        this.scoreA++;
+    }
+    public void addPointToTeamB() {
+        this.scoreB++;
+    }
 }
 
